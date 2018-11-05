f5ac37c8d33fed6ecba20267e4f846683b800aaa
chenfinkel <finkelchen20@gmail.com>
2018-11-05 22:20:27 +0200

    First day <3 #2

---
+++ src/main/java/sample/Term.java
@@ -0,0 +1,12 @@
+package sample;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Term {
+
+    private String id;
+    private int df;
+    private HashMap<String, Integer> docs;
+
+}
